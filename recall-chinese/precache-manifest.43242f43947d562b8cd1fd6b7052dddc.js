08ec0611810e2f99f4f329d0214abff8fca96cad
Darwin Gautalius <darwin@datasaur.ai>
2020-05-02 20:32:59 +0700

    first commit

---
+++ recall-chinese/precache-manifest.43242f43947d562b8cd1fd6b7052dddc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42abfd635617c34e55b0229d8b894b13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c1c6f63d025a43c49d2",
+    "url": "/static/css/2.d0b4e66b.chunk.css"
+  },
+  {
+    "revision": "5ad0b3d389cf9270ffb8",
+    "url": "/static/css/main.2133cde5.chunk.css"
+  },
+  {
+    "revision": "4c1c6f63d025a43c49d2",
+    "url": "/static/js/2.bb264bc9.chunk.js"
+  },
+  {
+    "revision": "d954c147e3b31eb801595540df0ca7f9",
+    "url": "/static/js/2.bb264bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad0b3d389cf9270ffb8",
+    "url": "/static/js/main.48176c41.chunk.js"
+  },
+  {
+    "revision": "475f78de79d8b4914c5d",
+    "url": "/static/js/runtime-main.7ab5e16e.js"
+  }
+]);
